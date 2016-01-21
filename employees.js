441bc960cb6eb164618edf86b07914e56da6fe75
drewctate <drewctate@gmail.com>
2016-01-21 09:32:02 -0700

    Improving UI, adding form validation and employee deletion
    Also adding .gitignore

--- employees.js
+++ employees.js
@@ -11,11 +11,18 @@ function EmployeeCtrl($scope) {
             photoid: $scope.photoid,
             visibleDetails: false
         });
+        // clear form data
+        $scope.name = null;
+        $scope.title = null;
+        $scope.age = null;
+        $scope.hireDate = null;
+        $scope.photoid = null;
         // TODO take care of API stuff and ID
+
     };
 
-    $scope.removeEmployee = function () {
-        // TODO remove employee from scope.employees
+    $scope.removeEmployee = function (employee) {
+        $scope.employees.splice($scope.employees.indexOf(employee), 1);
     };
 
 }
