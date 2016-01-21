cd6039b62f7c98103dc29a5d6199a27d2c903ef7
drewctate <drewctate@gmail.com>
2016-01-21 10:07:42 -0700

    Employees' info can now be updated

--- employees.js
+++ employees.js
@@ -2,7 +2,19 @@
 
 function EmployeeCtrl($scope) {
     $scope.employees = [];
+    $scope.btnText = 'Add';
+    var updatedEmployee;
+
+    function clearFormData() {
+        $scope.name = null;
+        $scope.title = null;
+        $scope.age = null;
+        $scope.hireDate = null;
+        $scope.photoid = null;
+    }
+
     $scope.addEmployee = function () {
+
         $scope.employees.push({
             name: $scope.name,
             title: $scope.title,
@@ -11,12 +23,7 @@ function EmployeeCtrl($scope) {
             photoid: $scope.photoid,
             visibleDetails: false
         });
-        // clear form data
-        $scope.name = null;
-        $scope.title = null;
-        $scope.age = null;
-        $scope.hireDate = null;
-        $scope.photoid = null;
+        clearFormData();
         // TODO take care of API stuff and ID
 
     };
@@ -24,6 +31,33 @@ function EmployeeCtrl($scope) {
     $scope.removeEmployee = function (employee) {
         $scope.employees.splice($scope.employees.indexOf(employee), 1);
     };
+
+    $scope.updateEmployeeForm = function (employee) {
+        $scope.name = employee.name;
+        $scope.title = employee.title;
+        $scope.age = employee.age;
+        $scope.hireDate = employee.hireDate;
+        $scope.photoid = employee.photoid;
+        updatedEmployee = employee;
+        $scope.btnText = 'Update';
+        $scope.submit = $scope.updateEmployee
+        $rootScope.formVisible = true; // display form if hidden
+    }
+
+    $scope.updateEmployee = function () {
+        updatedEmployee.name = $scope.name;
+        updatedEmployee.title = $scope.title;
+        updatedEmployee.age = $scope.age;
+        updatedEmployee.hireDate = $scope.hireDate;
+        updatedEmployee.photoid = $scope.photoid;
+
+        clearFormData();
+        $scope.btnText = 'Add';
+        $scope.submit = $scope.addEmployee;
+    }
+
+    // assign submission addEmployee function by default
+    $scope.submit = $scope.addEmployee;
 
 }
 
