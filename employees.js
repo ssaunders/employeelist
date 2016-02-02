bac08d0c641f291a5ec1350d169bdf925f242b8b
Andrew Tate <drewctate@gmail.com>
2016-02-02 15:18:40 -0700

    Hooking up delete and add functions, adding generic profile pic, fixing styles
    Update employee and the routing still needs to be figured out.
    Sliding directive disabled because of difficulty with multiple list items (it works on just one)

--- employees.js
+++ employees.js
@@ -9,7 +9,7 @@ function NewCtrl (EmployeeList, $scope, $rootScope, $location) {
     $rootScope.edit = true;
     $scope.btnText = 'Add';
 
-    $scope.addEmployee = function (e) {
+    $scope.addEmployee = function () {
         var newEmployee = {
             name: $scope.form.name,
             photoId: $scope.form.photoId,
@@ -23,22 +23,21 @@ function NewCtrl (EmployeeList, $scope, $rootScope, $location) {
     $scope.submit = $scope.addEmployee;
 }
 
-function UpdateCtrl (EmployeeList, $scope, $rootScope, $http, $stateParams, $location) {
+function UpdateCtrl (EmployeeList, $scope, $rootScope, $http, $stateParams, $location, $interval) {
     $rootScope.edit = true;
     $scope.btnText = 'Update';
-    $scope.form = {};
-    var employee = EmployeeList.loadEmployeeByID($stateParams.employeeId, function (response) {
-        $scope.form.name = response.data.name;
-        $scope.form.title = response.data.title;
-        $scope.form.age = response.data.age;
-        $scope.form.hireDate = new Date(response.data.hireDate);
-        $scope.form.photoId = response.data.photoId;
-    },
-    function () {});
+    $interval(function () {
+      var employee = EmployeeList.getEmployeeByID($stateParams.employeeId);
+      $scope.form = {};
+      $scope.form.name = employee.name;
+      $scope.form.title = employee.title;
+      $scope.form.age = employee.age;
+      $scope.form.hireDate = new Date(employee.hireDate);
+      $scope.form.photoId = employee.photoId;
+    }, 50, 8);
 
     $scope.updateEmployee = function () {
-        var employee = EmployeeList.loadEmployeeByID($stateParams.employeeId);
-        var localEmployee = EmployeeList.getEmployeeByID($stateParams.employeeId);
+        var employee = EmployeeList.getEmployeeByID($stateParams.employeeId);
         var updatedInfo = {
             name: $scope.form.name,
             photoId: $scope.form.photoId,
@@ -46,16 +45,16 @@ function UpdateCtrl (EmployeeList, $scope, $rootScope, $http, $stateParams, $loc
             hireDate: $scope.form.hireDate,
             title: $scope.form.title
         };
-        EmployeeList.updateEmployee($stateParams.employeeId, updatedInfo, function (response) {
+        console.log(updatedInfo);
+        $http.put('https://devapplications.mtc.byu.edu/training/v1/api/persons/' + employee.id, updatedInfo).then(function (response) {
             alert('Employee updated!');
-            localEmployee.name = response.data.name;
-            localEmployee.photoId = response.data.photoId;
-            localEmployee.age = response.data.age;
-            localEmployee.hireDate = response.data.hireDate;
-            localEmployee.title = response.data.title;
+            employee.name = $scope.form.name;
+            employee.photoId = $scope.form.photoId;
+            employee.age = $scope.form.age;
+            employee.hireDate = $scope.form.hireDate;
+            employee.title = $scope.form.title;
             $scope.btnText = 'Add';
             $location.path('/');
-            EmployeeList.refreshList();
         },
         function (response) {
             alert('Update failed ' + ' ' + response.status + ' ' + response.statusText);
@@ -65,11 +64,11 @@ function UpdateCtrl (EmployeeList, $scope, $rootScope, $http, $stateParams, $loc
     $scope.submit = $scope.updateEmployee;
 }
 
-function ListCtrl (EmployeeList, $scope, $http) {
-    EmployeeList.loadEmployees(function (response) {$scope.employees = response.data}, function() {alert("Employees couldn't be loaded.")}); // load initial employees
+function ListCtrl (EmployeeList, $interval, $scope, $http) {
+    $interval(function () {$scope.employees = EmployeeList.getEmployees()}, 250, 6); // load initial employees
 
     $scope.removeEmployee = function (employee) {
-        EmployeeList.deleteEmployee(employee.id, function () {
+        $http.delete('https://devapplications.mtc.byu.edu/training/v1/api/persons/' + employee.id).then(function () {
             $scope.employees.splice($scope.employees.indexOf(employee), 1);
         },
         function (response) {
@@ -80,35 +79,17 @@ function ListCtrl (EmployeeList, $scope, $http) {
 
 function EmployeeListService ($http) {
     var self = this;
-    loadEmployees(function (response) {self.employees = response.data;}, {});
+    $http.get('https://devapplications.mtc.byu.edu/training/v1/api/persons/').then(function (response) {
+        self.employees = response.data;
+    });
 
-    function getEmployeeByID(id) {
+    function getEmployeeByID (id) {
         for (var e of self.employees) {
             if (e.id === id) {
                 return e;
             }
         }
         return null;
-    }
-
-    function loadEmployees (success, fail) {
-        $http.get("https://devapplications.mtc.byu.edu/training/v1/api/persons/").then(success, fail);
-    }
-
-    function refreshList () {
-        loadEmployees(function (response) {self.employees = response.data;}, {});
-    }
-
-    function loadEmployeeByID (id, success, fail) {
-        $http.get("https://devapplications.mtc.byu.edu/training/v1/api/persons/" + id).then(success, fail);
-    }
-
-    function deleteEmployee (id, success, fail) {
-        $http.delete('https://devapplications.mtc.byu.edu/training/v1/api/persons/' + id).then(success, fail);
-    }
-
-    function updateEmployee (id, updatedInfo, success, fail) {
-        $http.put('https://devapplications.mtc.byu.edu/training/v1/api/persons/' + id, updatedInfo).then(success, fail);
     }
 
     function getEmployees () {
@@ -118,22 +99,20 @@ function EmployeeListService ($http) {
     function addNew(e) {
         $http.post('https://devapplications.mtc.byu.edu/training/v1/api/persons/', e).then(function (response) {
             self.employees.push(response.data);
+        },
+        function () {
+            alert("Employee couldn't be added");
         });
     }
 
     return {
         getEmployeeByID:getEmployeeByID,
-        loadEmployeeByID:loadEmployeeByID,
-        updateEmployee:updateEmployee,
         getEmployees:getEmployees,
-        loadEmployees:loadEmployees,
-        deleteEmployee:deleteEmployee,
-        addNew:addNew,
-        refreshList:refreshList
+        addNew:addNew
     };
 }
 
-var app = angular.module('EmployeeApp', ['ui.router']);
+var app = angular.module('EmployeeApp', ['ui.router', 'ngMaterial']);
 app.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/');
@@ -157,7 +136,51 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 });
 
 app.service('EmployeeList', ['$http', EmployeeListService])
-.controller('ListCtrl', ['EmployeeList', '$scope', '$http', ListCtrl])
+.controller('ListCtrl', ['EmployeeList', '$interval', '$scope', '$http', ListCtrl])
 .controller('MainCtrl', MainCtrl)
 .controller('NewCtrl', ['EmployeeList', '$scope', '$rootScope', '$location', NewCtrl])
-.controller('UpdateCtrl', ['EmployeeList', '$scope', '$rootScope', '$http', '$stateParams', '$location', UpdateCtrl]);
+.controller('UpdateCtrl', ['EmployeeList', '$scope', '$rootScope', '$http', '$stateParams', '$location', '$interval', UpdateCtrl]);
+// .directive('slideable', function () {
+//     return {
+//         restrict:'C',
+//         compile: function (element, attr) {
+//             // wrap tag
+//             var contents = element.html();
+//             element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
+//
+//             return function postLink(scope, element, attrs) {
+//                 // default properties
+//                 attrs.duration = (!attrs.duration) ? '0.5s' : attrs.duration;
+//                 attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+//                 element.css({
+//                     'overflow': 'hidden',
+//                     'height': '0px',
+//                     'transitionProperty': 'height',
+//                     'transitionDuration': attrs.duration,
+//                     'transitionTimingFunction': attrs.easing
+//                     });
+//                 };
+//             }
+//         };
+//     })
+//     .directive('slideToggle', function() {
+//         return {
+//             restrict: 'A',
+//             link: function(scope, element, attrs) {
+//                 var target = document.querySelector(attrs.slideToggle);
+//                 attrs.expanded = false;
+//                 element.bind('click', function() {
+//                     var content = target.querySelector('.slideable_content');
+//                     if(!attrs.expanded) {
+//                         content.style.border = '1px solid rgba(0,0,0,0)';
+//                         var y = content.clientHeight;
+//                         content.style.border = 0;
+//                         target.style.height = y + 'px';
+//                     } else {
+//                         target.style.height = '0px';
+//                     }
+//                     attrs.expanded = !attrs.expanded;
+//                 });
+//             }
+//         }
+//     });
